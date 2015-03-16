6e1857798a40ae381ebd4194a508b546daf06069
Fernando <fernando.cardenasm@gmail.com>
2015-03-16 10:58:16 -0500

    Add Day adapter class

---
+++ app/src/main/java/com/example/usuario/storm/adapters/DayAdapter.java
@@ -0,0 +1,67 @@
+package com.example.usuario.storm.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.usuario.storm.R;
+import com.example.usuario.storm.weather.Day;
+
+/**
+ * Created by usuario on 16/03/2015.
+ */
+public class DayAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days){
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; //we aren´t go to use this. Tag items for easy reference.
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null){
+            //brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; //public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}

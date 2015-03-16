8932ea3f36d91239d3f6e4c742b7efb3b3d3e78c
Fernando <fernando.cardenasm@gmail.com>
2015-03-15 23:58:35 -0500

    Finish costume daily list adapter

--- app/src/main/java/com/example/usuario/storm/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/usuario/storm/ui/DailyForecastActivity.java
@@ -2,6 +2,7 @@ package com.example.usuario.storm.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.usuario.storm.R;
 
@@ -13,6 +14,10 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+
+        setListAdapter(adapter);
     }
 
 

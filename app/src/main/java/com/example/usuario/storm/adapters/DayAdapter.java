34db2a81d8a139ba0e3b5367fd13e83c2d582b24
Fernando <fernando.cardenasm@gmail.com>
2015-03-16 15:28:34 -0500

    Add Recycler View in hourly layout

--- app/src/main/java/com/example/usuario/storm/adapters/DayAdapter.java
+++ app/src/main/java/com/example/usuario/storm/adapters/DayAdapter.java
@@ -61,7 +61,15 @@ public class DayAdapter extends BaseAdapter {
 
         holder.iconImageView.setImageResource(day.getIconId());
         holder.temperatureLabel.setText(day.getMaxTemperatureCelsius() + "");
-        holder.dayLabel.setText(day.getDayOfTheWeek());
+
+        if (position == 0){
+            holder.dayLabel.setText("Today");
+        }
+        else{
+            holder.dayLabel.setText(day.getDayOfTheWeek());
+        }
+
+
 
         return convertView;
     }
--- app/src/main/java/com/example/usuario/storm/ui/MainActivity.java
+++ app/src/main/java/com/example/usuario/storm/ui/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends ActionBarActivity implements LocationProvider.
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String DAILY_FORECAST = "DAILY_FORECAST";
+    public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
     private Forecast mForecast;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -313,6 +314,13 @@ public class MainActivity extends ActionBarActivity implements LocationProvider.
     public void startDailyActivity(View view){
         Intent intent = new Intent(this,DailyForecastActivity.class);
         intent.putExtra(DAILY_FORECAST,mForecast.getDailyForecast());
+        startActivity(intent);
+    }
+
+    @OnClick (R.id.hourlyButton)
+    public void startHourlyActivity(View view){
+        Intent intent = new Intent(this, HourlyForecastActivity.class);
+        intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
         startActivity(intent);
     }
 
